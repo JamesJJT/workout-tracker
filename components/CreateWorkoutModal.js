e23b75a639cd8282a06f803bc9175a3394f50025
James Stevens <jastevens2222@hotmail.co.uk>
2025-12-27 15:51:12 +0100

    Removed useless import

--- components/CreateWorkoutModal.js
+++ components/CreateWorkoutModal.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { View, Text, Modal, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 
 export default function CreateWorkoutModal({
--- components/SessionCard.js
+++ components/SessionCard.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default function SessionCard({ session, onEndSession, onCancelSession }) {
